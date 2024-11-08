9ef9507a88f7131f46b232087b847afcb2416472
out0f4mm0 <eriking2004@gmail.com>
2024-11-08 14:12:59 +0100

    Initial commit after fixing the mess I'd done

---
+++ api cerveza/src/test/java/org/example/apicerveza/ApiCervezaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apicerveza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCervezaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
